8b679713c2d0abbbddfe17e2f5b6445627067998
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-06-25 20:28:28 +0200

    sections

--- src/MumrichNet.K8s.Operators.MyOperator/Program.cs
+++ src/MumrichNet.K8s.Operators.MyOperator/Program.cs
@@ -1,8 +1,14 @@
 using KubeOps.Operator;
 
+// Builder
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddKubernetesOperator();
 
+// App
 var app = builder.Build();
+
 app.UseKubernetesOperator();
+
+// Run
 await app.RunOperatorAsync(args);
